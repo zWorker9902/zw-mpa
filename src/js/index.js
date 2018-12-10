35760a7893815002ca5c271eea0b722bf9346bd0
zWorker9902 <3240024992@qq.com>
2018-12-10 11:47:48 +0800

    [update 增加表格行扩展、列扩展、空书签隐藏]

--- src/js/index.js
+++ src/js/index.js
@@ -7,8 +7,7 @@ class Index {
   constructor(){
     $('#root').text('Index 你好，啦啦啦');
     this.init();
-    this.renderTableBookmark();
-    this.renderTable();
+
   }
 
   init() {
@@ -42,9 +41,15 @@ class Index {
       .then(res => {
         console.log('res:', res)
       })
+
+    $('#replaceData').on('click', () => {
+      this.renderTable1();
+      this.renderTable2();
+      this.renderTable3();
+    });
   }
 
- renderTableBookmark() {
+ renderTable1() {
    const bookmark = [
      [null, 2,    3, null, 5],
      [null, 2,    3, 4,    5],
@@ -112,7 +117,7 @@ class Index {
    $('.code-GP0061234567890').replaceWith(table);
  }
 
-  renderTable() {
+  renderTable2() {
     const bookmark = [
       ['姓名', "年龄", '张三', '李四', '王五'],
       [null, 4, null, 4, 4],
@@ -158,7 +163,6 @@ class Index {
           } else {
             tr.push(`<td>${k}</td>`);
           }
-
         }
       });
       tbody.push(`<tr>${tr.join("")}</tr>`);
@@ -166,6 +170,157 @@ class Index {
 
     const table = `<table><tbody>${tbody.join("")}</tbody></table>`;
     $('.code-GP0061234567891').replaceWith(table);
+  }
+
+  /**
+   * 特殊表格书签，按照行向下扩展
+   * @params(array) bookmark 书签数据
+   * @params(object) dom 被替换的dom元素
+   * */
+  replaceRowTableBMark(bookmark, dom) {
+    let tdDom = this.findParentsNode(dom, 'TD');
+    if (!tdDom) {
+      return null;
+    }
+    let trDom = tdDom.parentNode;
+    // 获取当前列索引
+    const tdIndex = tdDom.cellIndex;
+    // 获取当前行索引
+    const trIndex = trDom.rowIndex;
+    const tBody = trDom.parentNode;
+
+    bookmark.forEach((item, i) => {
+      if (i == 0) {
+        tdDom.innerHTML = item;
+      } else {
+        const nextTrDom = trDom.nextElementSibling;
+        // 结点已存在，并且已经替换过数据
+        if (nextTrDom && nextTrDom.className == 'replaced') {
+          nextTrDom.children[tdIndex].innerText = item;
+          // 移动到下一行数据
+          trDom = nextTrDom;
+        } else {
+          // 结点不存在，或者未替换数据
+          const newTrDom = trDom.cloneNode(true);
+
+          // 清空td单元格内部数据
+          const children = newTrDom.children;
+          for (let k = 0; k < children.length; k++) {
+            children[k].innerText = '';
+          }
+          children[tdIndex].innerText = item;
+          newTrDom.className = 'replaced';
+          // 在当前行的后面新插入一行数据
+          trDom.after(newTrDom);
+          // 移动到下一行数据
+          trDom = newTrDom;
+        }
+      }
+    });
+  }
+
+  /**
+   * 特殊表格书签，按照列向右扩展
+   * @params(array) bookmark 书签数据
+   * @params(object) dom 被替换的dom元素
+   * */
+  replaceColTableBMark(bookmark, dom) {
+    let tdDom = this.findParentsNode(dom, 'TD');
+    if (!tdDom) {
+      return null;
+    }
+    let trDom = tdDom.parentNode;
+    // 获取当前列索引
+    const tdIndex = tdDom.cellIndex;
+    // 获取当前行索引
+    const trIndex = trDom.rowIndex;
+    const tBody = trDom.parentNode;
+    const trDoms = tBody.children;
+
+    bookmark.forEach((item, i) => {
+      if (i == 0) {
+        tdDom.innerHTML = item;
+      } else {
+        const nextTdDom = tdDom.nextElementSibling;
+        // 结点已存在，并且已经替换过数据
+        if (nextTdDom && nextTdDom.className == 'replaced') {
+          nextTdDom.innerText = item;
+          // 移动到下一行数据
+          tdDom = nextTdDom;
+        } else {
+          // 结点不存在，或者未替换数据
+          // 清空td单元格内部数据
+          for(let k=0; k<trDoms.length; k++) {
+            const newTdDom = tdDom.cloneNode(true);
+            newTdDom.innerText = '';
+            newTdDom.className = 'replaced';
+
+            trDoms[k].children[tdIndex+i - 1].after(newTdDom);
+          }
+          // 填写数据信息
+          tdDom = trDoms[trIndex].children[tdIndex+i];
+          tdDom.innerText = item;
+        }
+      }
+    });
+  }
+
+  /**
+   * 普通书签替换，兼容在表格内部，为空则不展示
+   * @params(array) bookmark 书签数据
+   * @params(object) dom 被替换的dom元素
+   * */
+  replaceBMark(bookmark, dom) {
+    const tdDom = this.findParentsNode(dom, 'TD');
+    if (tdDom) {
+      let trDom = tdDom.parentNode;
+      if (trDom) {
+        bookmark === "" ? trDom.classList.add('hide') : trDom.classList.remove('hide');
+      }
+    }
+  }
+
+  renderTable3() {
+    const bookmark = [];
+    const bmarkName = [
+      '名称',
+      '张三',
+      '李四',
+    ];
+    const bmarkAge = [
+      '数量',
+      10,
+      20,
+      30,
+    ];
+    const tbody = [];
+    const colSpans = [];
+
+    this.replaceRowTableBMark(bmarkName, document.querySelector('.code-GP0061234567801'));
+    this.replaceRowTableBMark(bmarkAge, document.querySelector('.code-GP0061234567802'));
+    this.replaceRowTableBMark(bmarkName, document.querySelector('.code-GP0061234567803'));
+    this.replaceRowTableBMark(bmarkAge, document.querySelector('.code-GP0061234567804'));
+
+    this.replaceColTableBMark(bmarkName, document.querySelector('.code-GP0061234567805'));
+    this.replaceColTableBMark(bmarkAge, document.querySelector('.code-GP0061234567806'));
+
+    this.replaceBMark('', document.querySelector('.code-GP1'));
+    this.replaceBMark('', document.querySelector('.code-GP2'));
+  }
+
+  findParentsNode(dom, TAG) {
+    if (dom) {
+      const parentNode = dom.parentNode;
+      if (parentNode.tagName === 'BODY') {
+        return null;
+      }
+      if (parentNode.tagName === TAG) {
+        return parentNode;
+      } else {
+        dom = this.findParentsNode(parentNode, TAG);
+      }
+    }
+    return dom;
   }
 }
 
